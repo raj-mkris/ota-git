b7fbd31549cdc2b12aecfec4cfbfe56bcba089ed
Aakash <mkris.internal@gmail.com>
2023-08-07 13:16:41 +0530

    build

--- main/simple_ota_example.c
+++ main/simple_ota_example.c
@@ -26,7 +26,7 @@
 #endif
 
 #define HASH_LEN 32
-#define Version 1.0.0
+#define Version 1
 #ifdef CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF
 /* The interface name value can refer to if_desc in esp_netif_defaults.h */
 #if CONFIG_EXAMPLE_FIRMWARE_UPGRADE_BIND_IF_ETH
@@ -124,7 +124,7 @@ void simple_ota_example_task(void *pvParameter)
     else
     {
         ESP_LOGE(TAG, "Firmware upgrade failed");
-        ESP_LOGE("TAG", Version);
+        printf(Version);
     }
     while (1)
     {
